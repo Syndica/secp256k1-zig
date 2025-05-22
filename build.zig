c4000b7fd3e02dabfa2619489918905e859019ec
David Rubin <david@vortan.dev>
2025-05-21 19:44:17 -0700

    cleanup

--- build.zig
+++ build.zig
@@ -5,15 +5,6 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const upstream = b.dependency("secp256k1", .{});
-    const lib = b.addStaticLibrary(.{
-        .name = "libsecp256k1",
-        .target = target,
-        .optimize = optimize,
-    });
-    lib.linkLibC();
-
-    lib.addIncludePath(upstream.path("src"));
-
     const translate_c = b.addTranslateC(.{
         .root_source_file = upstream.path("include/secp256k1_recovery.h"),
         .target = target,
@@ -35,6 +26,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = true,
     });
+    b.installArtifact(libsecp256k1);
 
     libsecp256k1.addIncludePath(upstream.path("include"));
     libsecp256k1.addCSourceFiles(.{
@@ -46,7 +38,6 @@ pub fn build(b: *std.Build) void {
             "src/precomputed_ecmult_gen.c",
         },
     });
-
     mod.linkLibrary(libsecp256k1);
 }
 
