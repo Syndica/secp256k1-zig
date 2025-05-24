1d325dc533af14d5ffe6c70abb7cbd7d2ebfc5b2
David Rubin <david@vortan.dev>
2025-05-24 06:38:20 -0700

    cleanup + add pic option

--- build.zig
+++ build.zig
@@ -4,11 +4,36 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const pic = b.option(bool, "pic", "Build library with PIC enabled");
+
     const upstream = b.dependency("secp256k1", .{});
     const translate_c = b.addTranslateC(.{
         .root_source_file = upstream.path("include/secp256k1_recovery.h"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const lib = b.addLibrary(.{
+        .name = "secp256k1",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .pic = pic,
+        }),
+        .linkage = .static,
+    });
+    b.installArtifact(lib);
+
+    lib.linkLibC();
+    lib.addIncludePath(upstream.path("include"));
+    lib.addCSourceFiles(.{
+        .root = upstream.path("."),
+        .flags = FLAGS,
+        .files = &.{
+            "src/secp256k1.c",
+            "src/precomputed_ecmult.c",
+            "src/precomputed_ecmult_gen.c",
+        },
     });
 
     const mod = b.addModule("secp256k1", .{
@@ -17,28 +42,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = true,
     });
-
-    mod.addIncludePath(upstream.path("include"));
-
-    const libsecp256k1 = b.addStaticLibrary(.{
-        .name = "secp256k1",
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-    b.installArtifact(libsecp256k1);
-
-    libsecp256k1.addIncludePath(upstream.path("include"));
-    libsecp256k1.addCSourceFiles(.{
-        .root = upstream.path(&.{}),
-        .flags = FLAGS,
-        .files = &.{
-            "src/secp256k1.c",
-            "src/precomputed_ecmult.c",
-            "src/precomputed_ecmult_gen.c",
-        },
-    });
-    mod.linkLibrary(libsecp256k1);
+    mod.linkLibrary(lib);
 }
 
 const FLAGS = &.{
